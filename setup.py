997242d0e62bdef0f9d2983d1f758f268a428a7a
sbaker-dev <samuelbaker.researcher@gmail.com>
2021-03-21 22:16:49 +0000

    setup

---
+++ setup.py
@@ -0,0 +1,55 @@
+# Copyright (C) 2020 Samuel Baker
+
+DESCRIPTION = "Taking multiple output forms into journal tables"
+LONG_DESCRIPTION = """
+# pyJournalTables
+
+Taking multiple output forms into journal tables
+
+"""
+LONG_DESCRIPTION_CONTENT_TYPE = "text/markdown"
+
+DISTNAME = 'pyJournalTables'
+MAINTAINER = 'Samuel Baker'
+MAINTAINER_EMAIL = 'samuelbaker.researcher@gmail.com'
+LICENSE = 'MIT'
+DOWNLOAD_URL = "https://github.com/sbaker-dev/pyJournalTables"
+VERSION = "0.01.0"
+PYTHON_REQUIRES = ">=3.7"
+
+INSTALL_REQUIRES = [
+    'miscSupports',
+    ]
+
+CLASSIFIERS = [
+    'Programming Language :: Python :: 3.7',
+    'License :: OSI Approved :: MIT License',
+]
+
+if __name__ == "__main__":
+
+    from setuptools import setup, find_packages
+
+    import sys
+
+    if sys.version_info[:2] < (3, 7):
+        raise RuntimeError("pyJournalTables requires python >= 3.7.")
+
+    setup(
+        name=DISTNAME,
+        author=MAINTAINER,
+        author_email=MAINTAINER_EMAIL,
+        maintainer=MAINTAINER,
+        maintainer_email=MAINTAINER_EMAIL,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        long_description_content_type=LONG_DESCRIPTION_CONTENT_TYPE,
+        license=LICENSE,
+        version=VERSION,
+        download_url=DOWNLOAD_URL,
+        python_requires=PYTHON_REQUIRES,
+        install_requires=INSTALL_REQUIRES,
+        include_package_data=True,
+        packages=find_packages(),
+        classifiers=CLASSIFIERS
+    )
