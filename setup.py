618429c68564ba20d82fedf6c39b8a2e1b3b121f
sbaker-dev <samuelbaker.researcher@gmail.com>
2021-03-24 09:59:49 +0000

    version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ MAINTAINER = 'Samuel Baker'
 MAINTAINER_EMAIL = 'samuelbaker.researcher@gmail.com'
 LICENSE = 'MIT'
 DOWNLOAD_URL = "https://github.com/sbaker-dev/pyJournalTables"
-VERSION = "0.02.1"
+VERSION = "0.02.2"
 PYTHON_REQUIRES = ">=3.7"
 
 INSTALL_REQUIRES = [
