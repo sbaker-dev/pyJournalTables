e6759bac0b9e8ed1b0d9eb4b25b684be473c7811
sbaker-dev <samuelbaker.researcher@gmail.com>
2021-03-24 12:02:11 +0000

    Generalise some of the content in Regressions in RegCommon

--- pyJournalTables/Tables/Regressions.py
+++ pyJournalTables/Tables/Regressions.py
@@ -1,7 +1,76 @@
 from ..StataParser import StataCommon
 
+from csvObject import write_csv
 
-class OLS:
+
+class RegCommon:
+    def __init__(self, common_object):
+        """
+        Attributes found within the body of the OLS like tables
+
+        :param common_object: An object that contains all potential output from a log file
+        :type common_object: StataCommon
+        """
+
+        # Initialise the values that should be found in the table body for this table
+        self.coefficients = None
+        self.std_errs = None
+        self.t_stats = None
+        self.p_stats = None
+        self.conf_95_min = None
+        self.conf_95_max = None
+
+        # Set variable names
+        self.phenotype = common_object.phenotype
+        self.variables = common_object.variables
+
+    def conf_interval(self, var_key, rounding=4):
+        """Set the confidence interval as a string of str((Min_CI; Max_CI)), where the CI's can be rounded"""
+        return f"({round(self.conf_95_min[var_key], rounding)}; {round(self.conf_95_max[var_key], rounding)})"
+
+    def format_for_forest(self, write_directory=None, name=None, exclusions=None, as_csv=True):
+        """
+        Extract information that would be used to construct a forest plot figure
+
+        Note
+        ----
+        In future should have a use_blend option to just create the figure itself and cleanup all the files
+
+        :param write_directory: Where to write the data to if writing as csv
+        :type write_directory: str | Path | None
+
+        :param name: What to call the csv file if writing as csv
+        :type name: str | None
+
+        :param exclusions: A list of names to exclude, for example _cons. Defaults to None
+        :type exclusions: list[str] | None
+
+        :param as_csv: Write the information out as a csv
+        :type as_csv: bool
+
+        :return: Rows that where to be written out or None if they where written
+        :rtype: None | list[list]
+        """
+
+        # Select which variables we wish to isolate
+        if exclusions is None:
+            row_indexes = [i for i, _ in enumerate(self.variables)]
+        else:
+            row_indexes = [i for i, var in enumerate(self.variables) if var not in exclusions]
+
+        # Isolate each rows values
+        output = []
+        for index, (var, coef, lb, ub) in enumerate(
+                zip(self.variables, self.coefficients, self.conf_95_min, self.conf_95_max)):
+            if index in row_indexes:
+                output.append([var, coef, lb, ub])
+        if as_csv:
+            write_csv(write_directory, name, ["Phenotype", "Coefficient", "Lower Bound", "Upper Bound"], output)
+        else:
+            return output
+
+
+class OLS(RegCommon):
     def __init__(self, common_object):
         """
         OLS specific output
@@ -14,6 +83,7 @@ class OLS:
         :param common_object: An object that contains all potential output from a log file
         :type common_object: StataCommon
         """
+        super().__init__(common_object)
 
         # Load the common object
         self.common_obj = common_object
@@ -29,31 +99,15 @@ class OLS:
         self.adj_r_sqr = self.common_obj.adj_r_sqr
         self.root_mse = self.common_obj.root_mse
 
-        # Initialise the values that should be found in the table body for this table
-        self.coefficients = None
-        self.std_errs = None
-        self.t_stats = None
-        self.p_stats = None
-        self.conf_95_min = None
-        self.conf_95_max = None
-
         # Set the body attributes
         [setattr(self, h, [line[i] for line in self.common_obj.body]) for i, h in enumerate(self._body_headers)]
-
-        # Set variable names
-        self.phenotype = self.common_obj.phenotype
-        self.variables = self.common_obj.variables
 
     def __repr__(self):
         """Print the regression to show what is within it"""
         return f"{self.phenotype} = {' + '.join(v for v in self.variables)}"
 
-    def conf_interval(self, var_key, rounding=4):
-        """Set the confidence interval as a string of str((Min_CI; Max_CI)), where the CI's can be rounded"""
-        return f"({round(self.conf_95_min[var_key], rounding)}; {round(self.conf_95_max[var_key], rounding)})"
 
-
-class HDFE:
+class HDFE(RegCommon):
     def __init__(self, common_object):
         """
         HDFE specific output
@@ -68,6 +122,7 @@ class HDFE:
         :param common_object: An object that contains all potential output from a log file
         :type common_object: StataCommon
         """
+        super().__init__(common_object)
 
         # Load the common object
         self.common_obj = common_object
@@ -85,25 +140,9 @@ class HDFE:
         self.root_mse = self.common_obj.root_mse
         self.adjusted_clusters = self.common_obj.adjusted_clusters
 
-        # Initialise the values that should be found in the table body for this table
-        self.coefficients = None
-        self.std_errs = None
-        self.t_stats = None
-        self.p_stats = None
-        self.conf_95_min = None
-        self.conf_95_max = None
-
         # Set the body attributes
         [setattr(self, h, [line[i] for line in self.common_obj.body]) for i, h in enumerate(self._body_headers)]
-
-        # Set variable names
-        self.phenotype = self.common_obj.phenotype
-        self.variables = self.common_obj.variables
 
     def __repr__(self):
         """Print the regression to show what is within it"""
         return f"{self.phenotype} = {' + '.join(v for v in self.variables)}"
-
-    def conf_interval(self, var_key, rounding=4):
-        """Set the confidence interval as a string of str((Min_CI; Max_CI)), where the CI's can be rounded"""
-        return f"({round(self.conf_95_min[var_key], rounding)}; {round(self.conf_95_max[var_key], rounding)})"
