60cbb8240d694c574102fcf4e5a14b45b940baf8
sbaker-dev <samuelbaker.researcher@gmail.com>
2021-08-04 12:16:48 +0100

    Fix broken negative index setter

--- stataLogObject/Configs/Extractors.py
+++ stataLogObject/Configs/Extractors.py
@@ -11,15 +11,17 @@ class ExtractBody:
     skip_indexes: List = field(default_factory=lambda: [])
 
     def format_indexes(self, table_length):
-        """We may need to subtract indexes from the bottom, in which case the index is relative to explanatory variables"""
+        """
+        We may need to subtract indexes from the bottom, in which case the index is relative to the table length
 
-        # # TODO ???? Why is it minus 2
-        # print(table_length)
-        # for v in self.skip_indexes:
-        #     if v < 0:
-        #         print((table_length - 2) + v)
-
-        self.skip_indexes = [v if v >= 0 else (table_length - 2) + v for v in self.skip_indexes]
+        Note
+        ----
+        Table length is not base zero, hence why we need to take one way from it
+        negative skip indexes are formatted in the same way as indexing, so -1 retrieves the last element. However, if
+            we take the index of -1 from the table length then we always skip the second to last element. We can't use
+            zero, as zero is an actual index for the first element. As such, each negative value gets +1 added to it.
+        """
+        self.skip_indexes = [v if v >= 0 else (table_length - 1) + (v + 1) for v in self.skip_indexes]
 
 
 @dataclass
